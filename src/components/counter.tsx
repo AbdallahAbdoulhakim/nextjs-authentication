23de0833ce7dd6acc1927e7f9c4f49a33eb015b7
Abdallah Abdoulhakim <aabdoulhakim@gmail.com>
2025-05-02 13:55:30 +0300

    new commit

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { use, useState } from "react";
-import { useAuth, useUser } from "@clerk/nextjs";
+import { useState } from "react";
+import { useUser } from "@clerk/nextjs";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
 
   //const { isLoaded, userId, sessionId, getToken } = useAuth();
-  const { isLoaded, isSignedIn, user } = useUser();
+  const { isLoaded, isSignedIn } = useUser();
 
   if (!isLoaded || !isSignedIn) {
     return null;
